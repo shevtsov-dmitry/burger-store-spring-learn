b1e8fad22ba4285506e8bdef35cdf0b1faa44e7e
shado <shadowstakeus28@gmail.com>
2023-06-30 16:31:02 +0300

    Initial commit

---
+++ src/main/java/com/burger_store/BurgerStoreApplication.java
@@ -0,0 +1,13 @@
+package com.burger_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BurgerStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BurgerStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/burger_store/BurgerStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.burger_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BurgerStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
