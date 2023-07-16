dd25f973d373df6c1ba159111dd73dc951e973ab
shado <shadowstakeus28@gmail.com>
2023-07-16 18:56:48 +0300

    fix: schema.sql deleted foreign key ingredients_id in burger table

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,7 +5,6 @@ CREATE TABLE IF NOT EXISTS burger
     id             SERIAL PRIMARY KEY NOT NULL,
     name           varchar(10),
     datecreated    DATE,
-    ingredients_id INT,
 	order_id 	   INT
 );
 
@@ -31,10 +30,9 @@ CREATE TABLE IF NOT EXISTS ingredients
     mayonnaise boolean,
     ketchup    boolean
 );
-https://marketplace.eclipse.org/marketplace-client-intro?mpc_install=507775
+
 -- ADD FOREIGN KEYS
 
 ALTER TABLE burger ADD FOREIGN KEY(order_id) REFERENCES "order" (id);
-ALTER TABLE burger ADD FOREIGN KEY (ingredients_id) REFERENCES ingredients (id);
 ALTER TABLE ingredients ADD FOREIGN KEY (burger_id) REFERENCES burger(id);
 
