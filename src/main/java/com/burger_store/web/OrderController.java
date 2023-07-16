d3cde61d13309f7e6eb49f8a779c03dac0c5be93
shado <shadowstakeus28@gmail.com>
2023-07-16 18:57:24 +0300

    feat: makeOrder() save order details in database.

--- src/main/java/com/burger_store/web/OrderController.java
+++ src/main/java/com/burger_store/web/OrderController.java
@@ -1,13 +1,13 @@
 package com.burger_store.web;
 
 import com.burger_store.data.BurgerRepository;
+import com.burger_store.data.IngredientRepository;
 import com.burger_store.data.OrderRepository;
 import com.burger_store.samples.Burger;
 import com.burger_store.samples.Order;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -18,22 +18,18 @@ import org.springframework.web.bind.support.SessionStatus;
 @RequestMapping("makeOrder")
 public class OrderController {
 
-	private final JdbcTemplate jdbc;
 	private OrderRepository orderRepository;
 	private BurgerRepository burgerRepo;
+	private IngredientRepository ingredientRepo;
 	private static final Logger log = LoggerFactory.getLogger(OrderController.class);
 
 	@Autowired
-	public OrderController(JdbcTemplate jdbc, OrderRepository orderRepository, BurgerRepository burgerRepo) {
-		this.jdbc = jdbc;
+	public OrderController(OrderRepository orderRepository,
+						   BurgerRepository burgerRepo, IngredientRepository ingredientRepo) {
 		this.orderRepository = orderRepository;
 		this.burgerRepo = burgerRepo;
+		this.ingredientRepo = ingredientRepo;
 	}
-
-//	@ModelAttribute(name="order")
-//	public Order createOrder(){
-//		return new Order();
-//	}
 
 	@GetMapping
 	public String showForm(Model model) {
@@ -44,15 +40,13 @@ public class OrderController {
 	}
 
 	@PostMapping
-	public String makeOrder(@ModelAttribute Order order,
-							@ModelAttribute Burger burger,
-							SessionStatus session){
-//		log.info(order.getCity());
-//		log.info(order.getStreet());
-//		log.info(order.getApartment());
+	public String makeOrder(@ModelAttribute("order") Order order, SessionStatus session){
 		orderRepository.save(order);
-		burgerRepo.save(order.getOrderComponents(), order.getId());
-		log.info(order.getId().toString());
+		for (Burger burger: order.getOrderComponents()) {
+			burgerRepo.save(burger, order.getId());
+			ingredientRepo.save(burger, burger.getId());
+		}
+
 		session.setComplete();
 		return "redirect:/";
 	}
