accd0eda5bcc9d3fe53c90ec574adebcbf9e2134
shado <shadowstakeus28@gmail.com>
2023-08-01 11:37:00 +0300

    add: java main folder and StaffingApplication.java

---
+++ staffing-spring-data-jpa/src/main/java/com/staffing/StaffingApplication.java
@@ -0,0 +1,11 @@
+package com.staffing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaffingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StaffingApplication.class, args);
+    }
+}
