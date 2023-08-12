cb79737e1cca2afc9bbd7264a236695085e7e401
shado <shadowstakeus28@gmail.com>
2023-08-12 17:22:49 +0300

    restore: recreate video player project. Maintain cassandra db

---
+++ stream-video-player/src/main/java/com/streamvideoplayer/StreamVideoPlayerApplication.java
@@ -0,0 +1,13 @@
+package com.streamvideoplayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamVideoPlayerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamVideoPlayerApplication.class, args);
+    }
+
+}
---
+++ stream-video-player/src/test/java/com/streamvideoplayer/StreamVideoPlayerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.streamvideoplayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamVideoPlayerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
